d5266cb84c26e052d342b8ffc0cabcf4593a91b6
Cody Johnson <codyjohnsontx@gmail.com>
2024-06-26 18:28:56 -0500

    add flatlist and type safe components

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,23 @@
 import Colors from '@/src/constants/Colors';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import products from '@/assets/data/products';
+import { Product } from '../types';
+
+export const defaultPizzaImage = 
+    'https://notjustdev-dummy.s3.us-east-2.amazonnaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product;
+}
 
 
-const ProductListItem =({ product }) => {
+const ProductListItem =({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image}/>
+      <Image 
+      source={{ uri: product.image || defaultPizzaImage }} 
+      style={styles.image}
+      resizeMode='contain'
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -22,6 +33,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
   },
   title: {
     fontSize: 18,
